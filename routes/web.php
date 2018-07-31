78e061edb048a1a6e9505b63c2d8d5bcd17dc354
Werner Barnard <wbarnard81@gmail.com>
2018-07-31 10:28:46 +0200

    Added the first routes and page views

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('welcome');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/computers', 'ComputersController@index')->name('computers');
