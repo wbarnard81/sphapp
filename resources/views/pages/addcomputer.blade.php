e2b24d4a329f250703669350b2725552899b078b
Werner Barnard <wbarnard81@gmail.com>
2018-10-01 09:22:39 +0200

    Updated select fields for add computer form

--- resources/views/pages/addcomputer.blade.php
+++ resources/views/pages/addcomputer.blade.php
@@ -27,13 +27,14 @@
         {{Form::label('cpumodel', 'CPU Model')}} {{Form::text('cpumodel', '', ['class' => 'form-control', 'placeholder' => '...'])}}
     </div>
     <div class="form-group">
-        {{Form::label('memory', 'Memory')}} {{ Form::select('memory', ['1' => '1GB', '2' => '2GB', '3' => '3GB', '4' => '4GB', '6'
-        => '6GB', '8' => '8GB', '10' => '10GB', '12' => '12GB', '16' => '16GB', '24' => '24GB', '32' => '32GB'], null, ['class'
-        => 'form-control']) }}
+        {{Form::label('memory', 'Memory')}} {{ Form::select('memory', ['1GB' => '1GB', '2GB' => '2GB', '3GB' => '3GB', '4GB' => '4GB',
+        '6GB' => '6GB', '8GB' => '8GB', '10GB' => '10GB', '12GB' => '12GB', '16GB' => '16GB', '24GB' => '24GB', '32GB' =>
+        '32GB'], null, ['class' => 'form-control']) }}
     </div>
     <div class="form-group">
-        {{Form::label('officetype', 'Office Package')}} {{ Form::select('officetype', ['2010' => 'MS Office HB 2010', '2013' => 'MS
-        Office HB 2013', '2016' => 'MS Office HB 2016', '365' => 'MS Office 365'], null, ['class' => 'form-control']) }}
+        {{Form::label('officetype', 'Office Package')}} {{ Form::select('officetype', ['MS Office HB 2010' => 'MS Office HB 2010',
+        'MS Office HB 2013' => 'MS Office HB 2013', 'MS Office HB 2016' => 'MS Office HB 2016', 'MS Office 365' => 'MS Office
+        365'], null, ['class' => 'form-control']) }}
     </div>
     <div class="form-group">
         {{Form::label('officekey', 'Product Key')}} {{Form::text('officekey', '', ['class' => 'form-control', 'placeholder' => '...'])}}
