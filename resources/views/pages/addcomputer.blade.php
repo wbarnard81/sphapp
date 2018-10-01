f175f96f6b75f80eaf7f00b3f9d2b19a1dbfdbc8
Werner Barnard <wbarnard81@gmail.com>
2018-10-01 09:18:18 +0200

    Added drop down lists for addcomputer

--- resources/views/pages/addcomputer.blade.php
+++ resources/views/pages/addcomputer.blade.php
@@ -27,11 +27,13 @@
         {{Form::label('cpumodel', 'CPU Model')}} {{Form::text('cpumodel', '', ['class' => 'form-control', 'placeholder' => '...'])}}
     </div>
     <div class="form-group">
-        {{Form::label('memory', 'Memory')}} {{Form::text('memory', '', ['class' => 'form-control', 'placeholder' => '...'])}}
+        {{Form::label('memory', 'Memory')}} {{ Form::select('memory', ['1' => '1GB', '2' => '2GB', '3' => '3GB', '4' => '4GB', '6'
+        => '6GB', '8' => '8GB', '10' => '10GB', '12' => '12GB', '16' => '16GB', '24' => '24GB', '32' => '32GB'], null, ['class'
+        => 'form-control']) }}
     </div>
     <div class="form-group">
-        {{Form::label('officetype', 'Office Package')}} {{Form::text('officetype', '', ['class' => 'form-control', 'placeholder'
-        => '...'])}}
+        {{Form::label('officetype', 'Office Package')}} {{ Form::select('officetype', ['2010' => 'MS Office HB 2010', '2013' => 'MS
+        Office HB 2013', '2016' => 'MS Office HB 2016', '365' => 'MS Office 365'], null, ['class' => 'form-control']) }}
     </div>
     <div class="form-group">
         {{Form::label('officekey', 'Product Key')}} {{Form::text('officekey', '', ['class' => 'form-control', 'placeholder' => '...'])}}
